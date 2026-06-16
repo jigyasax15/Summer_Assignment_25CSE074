05aa64f79635a88dbcea73801d5a470c1ed4d846
jigyasax15 <jigyasamishra2007@gmail.com>
2026-06-16 12:23:52 +0530

    Day 3 assignment

---
+++ 09_prob.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1)
+        flag = 0;
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("Prime Number\n");
+    else
+        printf("Not a Prime Number\n");
+
+    return 0;
+}
---
+++ 10_prob.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, i, j, prime;
+
+    printf("Enter starting and ending numbers: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers are:\n");
+
+    for (i = start; i <= end; i++) {
+        if (i < 2)
+            continue;
+
+        prime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+
+        if (prime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
