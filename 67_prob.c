e6a398a1d56cf8c005f6d1e46516fbbdc20dd9bc
jigyasax15 <jigyasamishra2007@gmail.com>
2026-06-30 20:40:15 +0530

    Day 17 assignment

---
+++ 67_prob.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, i, j;
+
+    printf("Enter the number of elements in first array: ");
+    scanf("%d", &n1);
+
+    int a[n1];
+
+    printf("Enter the elements of first array:\n");
+    for(i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter the number of elements in second array: ");
+    scanf("%d", &n2);
+
+    int b[n2];
+
+    printf("Enter the elements of second array:\n");
+    for(i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+
+    printf("Intersection of Arrays: ");
+
+    for(i = 0; i < n1; i++)
+    {
+        for(j = 0; j < n2; j++)
+        {
+            if(a[i] == b[j])
+            {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
