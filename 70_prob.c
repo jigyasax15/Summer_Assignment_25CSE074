21f4c6aa81c1f3c2c580f92b0cdb055d261fc2ad
jigyasax15 <jigyasamishra2007@gmail.com>
2026-07-01 21:19:31 +0530

    Day 18 assignment

---
+++ 70_prob.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, minIndex, temp;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Original Array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    for(i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    printf("\nSorted Array (Ascending Order): ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
